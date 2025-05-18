0af03da6a3df9cc7ffa5f1a46efed8997c10de9c
Morgan Maina <61865560+MorganMaina@users.noreply.github.com>
2025-05-18 22:41:59 +0300

    Update Logic.sol

--- Logic.sol
+++ Logic.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-/// @title Logic Contract
-/// @notice Contains actual logic to be used behind proxy
+///  Logic Contract
+/// Contains actual logic to be used behind proxy
 contract Logic {
     uint256 public number;
 
